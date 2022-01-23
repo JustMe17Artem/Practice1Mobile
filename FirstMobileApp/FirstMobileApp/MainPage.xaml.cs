c336103a7dfef9191406083df8061d362dc0b4cf
unknown <sematkinartem6@gmail.com>
2022-01-23 23:07:11 +0300

    auth and reg page created

---
+++ FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace FirstMobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Projects_Page());
+        }
+
+        private async void Reg_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegPage());
+        }
+    }
+}
